434e7803ef00edfe83875aa731d20a1c0b98994c
Katja Glaß <katja.glass@glacon.eu>
2022-02-04 16:21:36 +0100

    Start on working About

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,7 +3,13 @@ import Stack from '@mui/material/Stack';
 import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
 
+import aboutBoard from '../assets/content/about_board.md';
+
 const About: React.FC = () => {
+  // https://stackoverflow.com/questions/44678315/how-to-import-markdown-md-file-in-typescript
+
+  console.log(aboutBoard);
+
   return (
     <Stack sx={{ m: 4 }} spacing={2}>
       <Typography variant='h5' color='grey.600'>
