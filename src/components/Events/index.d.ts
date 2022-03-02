b5417b3422e636edfcdb80286094f50ca68c7dca
Katja Glass Consulting <55694577+KatjaGlassConsulting@users.noreply.github.com>
2022-03-02 22:43:41 +0100

    Revert "Migrate to new version of cosa page"

--- src/components/Events/index.d.ts
+++
@@ -1,9 +0,0 @@
-export interface IEvent {
-  title: string;
-  type: 'webinar' | 'conference' | 'hackathon' | 'other';
-  description: string;
-  logo?: string;
-  startDate: string;
-  endDate?: string;
-  past?: boolean;
-}
