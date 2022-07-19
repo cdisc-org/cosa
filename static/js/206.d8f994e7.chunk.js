1e6bd04b1781214fa1c6ff8d08cfd08f517371a4
DmitryMK <dmitry.m.kolosov@gmail.com>
2022-07-19 12:21:29 +0000

    Deploying to gh-pages from @ cdisc-org/cosa@10d9accb543996645ae287818cc3f37ac7e2bd6a 🚀

---
+++ static/js/206.d8f994e7.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdirectory=self.webpackChunkdirectory||[]).push([[206],{3206:function(e,t,c){e.exports=c.p+"static/media/spotlight_Q3_2022.1cfd57bf80f57e287042.md"}}]);
